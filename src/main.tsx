b22acccf3fadfde2e585b9caa840c3f4b6a716a5
9pvbloo <i2413131@continental.edu.pe>
2026-09-12 18:20:42 -0500

    perf(fonts): load only latin font subsets

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import '@fontsource/archivo-black/400.css'
-import '@fontsource/space-grotesk/400.css'
-import '@fontsource/space-grotesk/500.css'
-import '@fontsource/ibm-plex-mono/400.css'
+import '@fontsource/archivo-black/latin-400.css'
+import '@fontsource/space-grotesk/latin-400.css'
+import '@fontsource/space-grotesk/latin-500.css'
+import '@fontsource/ibm-plex-mono/latin-400.css'
 
 import './index.css'
 import App from './App.tsx'
